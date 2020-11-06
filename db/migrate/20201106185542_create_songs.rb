2656d6ba376cba5517b3491216a38377cd931e6f
Matt Gordon <bigmattsize21@gmail.com>
2020-11-06 20:40:20 +0000

    Automatically backed up by Learn

--- db/migrate/20201106185542_create_songs.rb
+++ db/migrate/20201106185542_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :name
-      t.artist_id :integer
+      t.integer 
       t.genre_id :integer
 
       t.timestamps
